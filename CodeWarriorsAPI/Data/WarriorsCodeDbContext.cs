90c34d2aa5ccc7470a7775c73a17fac457291c12
YouSayLari <larissa@think4.com.br>
2023-02-24 17:06:58 -0300

    apagando WeatherForecast e adicionando dbContext

---
+++ CodeWarriorsAPI/Data/WarriorsCodeDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeWarriorsAPI.Data
+{
+    public class WarriorsCodeDbContext : DbContext
+    {
+        public WarriorsCodeDbContext(DbContextOptions<WarriorsCodeDbContext> options) : base(options) { }
+
+        
+    }
+}
